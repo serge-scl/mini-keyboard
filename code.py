2fb24e7450ab6e5acfe44f6e9e77a5c219304dce
sergesclv <sergesclv@gmail.com>
2026-07-20 11:07:28 +0300

    code_min

--- code.py
+++ code.py
@@ -1,22 +1,9 @@
-# Raspberry Pi Pico Keyboard Emulator
+# Raspberry Pi Pico 
 # Using CircuitPython
-# Using Adafruit USB_HID Librarys
 
 
 from sensor_hall import main
 import asyncio
-#import digitalio
-#import board
-#from sensor_touch import sens_touch
-
-
-
-# while True:
-
-# sens_touch()
-# st = sens_touch()
-
-#print("catch", st)
 
 asyncio.run(main())
     
